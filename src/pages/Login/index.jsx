6863d033622b13b46b7eaa70b7050d4ee2a749c2
Felipe Santos <felipek2267@gmail.com>
2024-05-11 00:51:00 -0300

    ajutando login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Password } from 'primereact/password';
 import { useState } from "react";
 import { Button } from "primereact/button";
+import { useForm } from "react-hook-form";
 
 const LoginContainer = styled.section`
   whidth: 100%;
@@ -39,21 +40,34 @@ const LoginContainer = styled.section`
 
 const Login = () => {
 
+    const {
+      register,
+      handleSubmit,
+    } = useForm();
+
     const [value, setValue] = useState('');
 
     return (
         <LoginContainer>
-          <form >
+          <form onSubmit={handleSubmit(onsubmit)}>
             <h1>LOGIN</h1>
-            <label htmlFor="usuario">Usuário</label>
-            <InputText 
-              id="nome"
-              placeholder="Digite seu Usuário"/>
+            <label htmlFor="email">Usuário</label>
+            <InputText
+              id="email"
+              placeholder="Digite seu Email"
+              {...register("email", {
+                required: true,
+              })}
+              />
             <label htmlFor="senha">Senha</label>
             <div className="card flex w-full">
-              <Password value={value} onChange={(e) => setValue(e.target.value)} toggleMask />
+              <Password value={value} onChange={(e) => setValue(e.target.value)} toggleMask required="true" 
+              {...register("password", {
+                required: true,
+              })}
+              />
             </div>
-            <Button label="Entrar"/>
+            <Button label="Entrar" type="submit"/>
           </form>
         </LoginContainer>
     );
